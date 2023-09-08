c907349c6297fd424397f6d894933b69b2f87aa6
guilherme <chaves.guilherme@proton.me>
2023-09-08 15:02:16 -0300

    Handling exceptions and clearing screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,18 +15,28 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            System.out.println();
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
 
-            System.out.print("Target: ");
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.print("Target: ");
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
-
     }
 }
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -2,10 +2,8 @@ package chess;
 
 import boardgame.BoardException;
 
-import java.io.Serial;
 
 public class ChessException extends BoardException {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public ChessException(String message) {
