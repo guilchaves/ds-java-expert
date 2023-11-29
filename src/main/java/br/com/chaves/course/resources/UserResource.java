fc76628b129b3f4e57a2025b15c1a68479951193
guilherme <chaves.guilherme@proton.me>
2023-11-29 17:47:56 -0300

    chore: User entity and resource

---
+++ src/main/java/br/com/chaves/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package br.com.chaves.course.resources;
+
+import br.com.chaves.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@email.com", "99999999", "12345");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
