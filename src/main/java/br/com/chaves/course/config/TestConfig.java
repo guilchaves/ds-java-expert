3e2e3396cf10ae75e68481d5d8f908f898ec2319
guilherme <chaves.guilherme@proton.me>
2023-11-30 16:54:02 -0300

    chore: OrderItem, many-to-many association with extra attributes

--- src/main/java/br/com/chaves/course/config/TestConfig.java
+++ src/main/java/br/com/chaves/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package br.com.chaves.course.config;
 
-import br.com.chaves.course.entities.Category;
-import br.com.chaves.course.entities.Order;
-import br.com.chaves.course.entities.Product;
-import br.com.chaves.course.entities.User;
+import br.com.chaves.course.entities.*;
 import br.com.chaves.course.entities.enums.OrderStatus;
-import br.com.chaves.course.repositories.CategoryRepository;
-import br.com.chaves.course.repositories.OrderRepository;
-import br.com.chaves.course.repositories.ProductRepository;
-import br.com.chaves.course.repositories.UserRepository;
+import br.com.chaves.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +23,10 @@ public class TestConfig implements CommandLineRunner {
     private CategoryRepository categoryRepository;
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -64,5 +62,11 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
