009cbb29b2cd719ddf321d943f567702712ade07
guilherme <chaves.guilherme@proton.me>
2023-11-30 17:30:36 -0300

    chore: Subtotal and Total methods

--- src/main/java/br/com/chaves/course/entities/Payment.java
+++ src/main/java/br/com/chaves/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package br.com.chaves.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
