1868ade8618f23488564c9c1698bb138915a19a9
guilherme <chaves.guilherme@proton.me>
2023-12-02 19:05:16 -0300

    chore: add user posts endpoint

--- src/main/java/br/com/chaves/mongo/resources/UserResource.java
+++ src/main/java/br/com/chaves/mongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.chaves.mongo.resources;
 
+import br.com.chaves.mongo.domain.Post;
 import br.com.chaves.mongo.domain.User;
 import br.com.chaves.mongo.dto.UserDTO;
 import br.com.chaves.mongo.services.UserService;
@@ -51,5 +52,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
