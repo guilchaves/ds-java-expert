59d7d29878f3e32e1e630b0259cfef3113b56947
guilherme <chaves.guilherme@proton.me>
2023-12-02 20:55:39 -0300

    chore: query with multiple params

--- src/main/java/br/com/chaves/mongo/services/PostService.java
+++ src/main/java/br/com/chaves/mongo/services/PostService.java
@@ -6,6 +6,7 @@ import br.com.chaves.mongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,13 +20,17 @@ public class PostService {
         Optional<Post> post = repo.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
-//
+
 //    public List<Post> findByTitle(String text){
 //        return repo.findByTitleContainingIgnoreCase(text);
 //    }
 
-
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
