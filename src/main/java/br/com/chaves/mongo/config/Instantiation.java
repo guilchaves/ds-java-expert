823b19f2d6898ca1011e63747852f992a40ef44d
guilherme <chaves.guilherme@proton.me>
2023-12-02 16:31:08 -0300

    chore: add database seeding

---
+++ src/main/java/br/com/chaves/mongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package br.com.chaves.mongo.config;
+
+import br.com.chaves.mongo.domain.User;
+import br.com.chaves.mongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com");
+        User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+    }
+}
