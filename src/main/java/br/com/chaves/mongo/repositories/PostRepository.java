73bef42997dfebcdd49b676279df7930c009d86d
guilherme <chaves.guilherme@proton.me>
2023-12-02 20:25:44 -0300

    chore: add query method

--- src/main/java/br/com/chaves/mongo/repositories/PostRepository.java
+++ src/main/java/br/com/chaves/mongo/repositories/PostRepository.java
@@ -3,5 +3,9 @@ package br.com.chaves.mongo.repositories;
 import br.com.chaves.mongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
